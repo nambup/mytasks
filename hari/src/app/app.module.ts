f1f32c079ab0a578ad1f1fe66265f13b970636a2
Harisankar S <itsmeharishankar@gmail.com>
2022-10-01 23:09:48 +0530

    initial commit

--- hari/src/app/app.module.ts
+++ hari/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NewcomponentComponent } from './components/newcomponent/newcomponent.component';
+import { Component2Component } from './components/component2/component2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewcomponentComponent,
+    Component2Component
   ],
   imports: [
     BrowserModule
---
+++ hari/src/app/components/newcomponent/newcomponent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newcomponent123',
+  templateUrl: './newcomponent.component.html',
+  styleUrls: ['./newcomponent.component.css']
+})
+export class NewcomponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
