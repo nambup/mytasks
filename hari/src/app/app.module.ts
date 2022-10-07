715c81f359e922deb3975342e90429ab07d40635
Harisankar S <itsmeharishankar@gmail.com>
2022-10-08 01:16:53 +0530

    initial commit

--- hari/src/app/app.module.ts
+++ hari/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NewcomponentComponent } from './components/newcomponent/newcomponent.component';
 import { Component2Component } from './components/component2/component2.component';
+import { Component3Component } from './component3/component3.component';
+import { Component4Component } from './component4/component4.component';
+import { Component5Component } from './component5/component5.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NewcomponentComponent,
-    Component2Component
+    Component2Component,
+    Component3Component,
+    Component4Component,
+    Component5Component
   ],
   imports: [
     BrowserModule
